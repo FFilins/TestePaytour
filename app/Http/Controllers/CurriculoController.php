d50f8b7bdabc3abb2afa2d884761df6cb6eaa514
FFilins <felipeclayton007@gmail.com>
2023-04-16 11:15:02 -0300

    enviar email abstraido em uma classe

--- app/Http/Controllers/CurriculoController.php
+++ app/Http/Controllers/CurriculoController.php
@@ -16,8 +16,19 @@ class CurriculoController extends Controller
 
     public function add(Request $request) 
     {
+        
 
         try {
+            $request->validate([
+                'nome' => 'required|string|max:255',
+                'email' => 'required|email|max:255',
+                'telefone' => 'required|string|max:20',
+                'cargo' => 'required|string|max:255',
+                'escolaridade' => 'required|string|max:30',
+                'observacoes' => 'nullable|string|max:255',
+                'arquivo' => 'required|mimes:pdf,doc,docx|max:1024'
+            ]);
+
             $curriculo = new Curriculo();
 
             $curriculo->nome = $request->nome;
@@ -29,11 +40,8 @@ class CurriculoController extends Controller
             $curriculo->ip = $request->ip();
 
             // dd($request->ip());
-            //validate file
-            if ($request->hasFile('arquivo') && $request->file('arquivo')->isValid()) {
-                $request->validate([
-                    'arquivo' => 'required|mimes:pdf,doc,docx|max:1024'
-                ]);
+            // validate file
+            if ($request->file('arquivo')->isValid()) {
                 
                 $arquivo = $request->arquivo;
                 $extension = $arquivo->extension();
@@ -42,15 +50,21 @@ class CurriculoController extends Controller
                 $curriculo->arquivo = $arquivoName;
                 
                 $curriculo->save();
+
                 $enviarCurriculoPorEmail = new EnviarCurriculoPorEmail($curriculo);
 
                 $enviarCurriculoPorEmail->enviar();
+                if($enviarCurriculoPorEmail->foiEnviado()) {
+                    flash('Currículo enviado com sucesso!');
+                    return redirect()->back();
+                }
+                
                 // $id = $curriculo->id;
                 // flash('Currículo enviado com sucesso!');
                 // return redirect()->route('email.enviar')->with(compact('id'));
+            }else {
+                throw new Exception('Arquivo não é válido!');
             }
-
-            throw new Exception('Erro: Arquivo não suportado!');
 
         }catch (Exception $e) {
             flash($e->getMessage())->error();
--- app/UseCases/EnviarCurriculoPorEmail.php
+++ app/UseCases/EnviarCurriculoPorEmail.php
@@ -13,28 +13,33 @@ class EnviarCurriculoPorEmail
 
     private string $email;
 
+    private bool $enviado = false;
+
     public function __construct(Curriculo $curriculo) 
     {
         $this->curriculo = $curriculo;
 
         $this->email = config('app.email');
+
     }
 
     public function enviar() 
     {
          // Configurar a conta para enviar o email no arquivo .env
-
-        try{
-
+        try {
             $enviarEmail = Mail::to($this->email)->send(new SendMail($this->curriculo));
-            dd($enviarEmail);
+            if ($enviarEmail) {
+                $this->enviado = true;
+            }
 
-            flash('Currículo enviado com sucesso!')->success();
-            return redirect()->intended('/'); 
-
-        }catch (Exception $e) {
-            flash($e->getMessage())->warning();
-            return redirect()->back();
+        }catch(Exception $e) {
+            throw new Exception('Erro ao enviar Email');
         }
+
+    }
+
+    public function foiEnviado() :bool 
+    {
+        return $this->enviado;
     }
 }
